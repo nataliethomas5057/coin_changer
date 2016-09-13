0013e77cb4ee2ba865fdc808bc03c9d8675626cc
Natalie Thomas <Natalie Thomas>
2016-09-13 15:57:35 -0400

    second test for nickel trial

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,3 +1,3 @@
 def coin_changer(cents)
-	{}
+	change = {:quarter=>"25"; :dime=>"10", :nickel=>"5", :penny="one"}
 end	
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -5,4 +5,10 @@ class TestCoinChanger < Minitest::Test
 	def test_0_cents_returns_empty_hash
 		assert_equal({}, coin_changer(0))
 	end
+
+	def test_5_cents_returns_nickel
+		assert_equal({nickel}, coin_changer(5)
+	end
+
+
 end		
